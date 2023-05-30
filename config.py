b0cbc58fc7c7234db7496eb1847e841430cf5b4d
yasarsultan <yasersultan03@gmail.com>
2023-05-30 23:53:14 +0530

    commit configurations

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+class ProductionConfig(config):
+    DEBUG = False
+
+class StagingConfig(config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(config):
+    TESTING = True
